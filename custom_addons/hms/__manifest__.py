be9fe6f8477dfad585a7fe9406d9c71b889190d5
WafaeyKhaled1 <wafaeykhaled732@gmail.com>
2026-05-23 15:25:18 +0300

    Add only custom HMS modules

---
+++ custom_addons/hms/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hospital Management System',
+    'version': '1.0',
+    'summary': 'Manage patients in a hospital',
+    'category': 'Healthcare',
+    'author': 'Wafaey',
+    'depends': ['base'],       
+    'data': [
+        'views/patient_views.xml',
+        'views/doctor_views.xml',
+        'views/department_views.xml',
+        'views/menu.xml',
+    ],     
+}
